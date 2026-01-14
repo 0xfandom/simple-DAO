9c9a991bb51b1bce608dbad46876189489caec66
0xfandom <kashyapshivank01@gmail.com>
2026-01-14 19:26:04 +0530

    feat: deploy script of treasury with timelock as executor

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Treasury is ReentrancyGuard {
+    // executor is typically the Timelock contract
     address public executor;
 
     constructor(address _executor) {
