d93c289f4329238fcbd951f1a46e9cdf80d0d542
0xfandom <kashyapshivank01@gmail.com>
2026-01-16 17:47:35 +0530

    feat: emit proposal executed events

--- contracts/governance/Executor.sol
+++ contracts/governance/Executor.sol
@@ -8,27 +8,15 @@ contract Executor {
     Governance public governance;
     DAOTimelock public timelock;
 
-    constructor(address _governance, DAOTimelock _timelock) {
-        governance = Governance(_governance);
-        timelock = _timelock;
+    event ProposalExecuted(uint256 indexed proposalId);
+
+    constructor(address _gov) {
+        governance = Governance(_gov);
     }
 
-    function executeQueued(uint256 proposalId) external {
-        Governance.Proposal memory p = governance.getProposal(proposalId);
+    function execute(uint256 proposalId) external {
+        governance.executeProposal(proposalId);
 
-        require(p.queued, "not queued");
-        require(!p.executed, "already executed");
-
-        bytes32 salt = keccak256(abi.encode(proposalId));
-
-        timelock.execute(
-            p.target,
-            p.value,
-            p.data,
-            bytes32(0),
-            salt
-        );
-
-        governance.markExecuted(proposalId);
+        emit ProposalExecuted(proposalId);
     }
 }
