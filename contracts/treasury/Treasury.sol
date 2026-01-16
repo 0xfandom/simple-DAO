78391ff61f7060541fa9a6e0dd08a5d3e75169fc
0xfandom <kashyapshivank01@gmail.com>
2026-01-16 11:46:56 +0530

    chore: add SafeERC20 support to treasury

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.20;
 
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+using SafeERC20 for IERC20;
+
 
 contract Treasury is ReentrancyGuard {
     // executor is typically the Timelock contract
